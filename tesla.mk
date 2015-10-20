d802c5c8cbefcb069c320fda1512ebffbfd10c90
Tushar Jain <catusharjain95@gmail.com>
2015-10-20 15:01:40 +0530

    Update and rename cm.mk to tesla.mk

--- tesla.mk
+++ tesla.mk
@@ -23,12 +23,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_cancro
+PRODUCT_NAME := tesla_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
